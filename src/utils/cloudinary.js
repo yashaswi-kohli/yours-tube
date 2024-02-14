cbba170e87500e10e15305d0699704d67b65afc2
Yashaswi Kohli <yashasvi1707@gmial.com>
2024-02-14 20:48:06 +0530

    cloudinary basic setup

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,33 @@
+import { v2 as cloudinary } from "cloudinary";
+import fs from "fs";
+          
+cloudinary.config({ 
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
+  api_key: process.env.CLOUDINARY_API_KEY, 
+  api_secret: process.env.CLOUDINARY_API_SECRET 
+});
+
+const uploadFileOnCloudinary = async (filePath) => {
+    try {
+        if(!filePath) return null;
+        
+        //* uploading the file on cloudinary
+        const response = await cloudinary.uploader.upload
+        (filePath, {
+                resource_type: "auto"
+        })
+
+        //* file has been uploaded successfully
+        console.log("file is uploaded on cloudinary, ", result);
+
+        return response;
+    } catch (error) {
+
+        //* remove the locally saved temp file from the server, as the upload got failed
+        fs.unlinkSync(filePath);
+
+        return null;
+    }
+}
+
+export {uploadFileOnCloudinary};
